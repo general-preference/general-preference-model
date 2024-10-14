092514f01e1754714e8221b733fbe8bc025bce2e
kirigayahitsugi <zhangge19951114@163.com>
2024-10-14 18:29:15 +0800

    modify rewardbench eval

--- rewardbench_eval/run_rm_rewardbench.sh
+++ rewardbench_eval/run_rm_rewardbench.sh
@@ -1,12 +1,12 @@
 export CUDA_VISIBLE_DEVICES=0
 python run_rm_rewardbench.py \
---model /cephfs/zhangge/root/code/sqz_general_preference/results/saved_model/gemma-2-2b-it/rm/batch32_tau01_no_sft_1e5_sky80k_cleaned_epoch2_vh8_w_moe_w_l2 \
+--model  general-preference/GPM-Gemma-2B \
 --chat_template raw \
 --bf16 \
 --flash_attn \
 --is_custom_model \
 --do_not_save \
---model_name "test" \
+--model_name "general-preference/GPM-Gemma-2B" \
 --batch_size 64 \
 --value_head_dim 8 \
 --max_length 4096 \
